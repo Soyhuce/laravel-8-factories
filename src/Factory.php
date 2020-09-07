d7ab96d1f8beed457ff92122521c21c18db71272
Bastien Philippe <bastien.philippe@soyhuce.fr>
2020-09-07 10:02:10 +0200

    Update sources

--- src/Factory.php
+++ src/Factory.php
@@ -325,6 +325,10 @@ abstract class Factory
                 return $this->parentResolvers();
             }], $states->all()));
         })->reduce(function ($carry, $state) use ($parent) {
+            if ($state instanceof Closure) {
+                $state = $state->bindTo($this);
+            }
+
             return array_merge($carry, $state($carry, $parent));
         }, $this->definition());
     }
